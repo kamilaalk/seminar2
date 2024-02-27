4808cf8c2e629fd4f146d51aa7f223725b03c67e
kamilaalk <160918049+kamilaalk@users.noreply.github.com>
2024-02-28 01:01:58 +0600

    Add files via upload
    add Task4

---
+++ Task4.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая на вход принимает натуральное число N,
+// а на выходе показывает его цифры через запятую.
+
+int num = 65;
+
+if (num < 10)
+{
+    Console.WriteLine(num);
+}
+
+else
+{
+    while (num > 0)
+    {
+        int currentDigit = num % 10;
+        num /= 10;
+        if (num > 0)
+        {
+            Console.Write(currentDigit + ",");
+        }
+        else
+        Console.WriteLine(currentDigit);
+    }
+}
+
