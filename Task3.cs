0a1ada70e7574056c22e8fd66925b1921bb0c863
kamilaalk <160918049+kamilaalk@users.noreply.github.com>
2024-02-28 00:16:20 +0600

    Add files via upload
    add Task3

---
+++ Task3.cs
@@ -0,0 +1,22 @@
+﻿// Задача 3: Напишите программу, которая принимает на вход целое число
+// из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+int a = 199;
+int first = a / 10;
+int second = a % 10;
+
+if (a < 10 || a > 99)
+{
+    Console.WriteLine("Ошибка: Значение \"а\" должно быть в пределах [10, 99]");
+}
+
+else if (first > second)
+{
+    Console.WriteLine(first);
+}
+
+if (first < second)
+{
+    Console.WriteLine(second);
+}
+
